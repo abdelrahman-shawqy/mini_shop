c44f6a099a43c176ab1878277e98c01e92a2bfc8
abdelrahman <aabdelrahmann.sshawqqy.1482000@gmail.com>
2026-01-30 16:46:16 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mini_shop/screens/Cart%20Screen.dart';
+import 'package:mini_shop/screens/ProductScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,7 +10,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
 
+
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      initialRoute: ProductScreen.routeName,
+      routes: {
+        ProductScreen.routeName:(context)=>ProductScreen(),
+        CartScreen.routName:(context)=>CartScreen(),
+      },
+    );
   }}
